bd87bbd358bbce000ad5aa21a98a258423cfa573
vusumuzi <vusumuzi11k@yahoo.co.uk>
2016-04-03 00:04:14 +0100

    Added event Handler

--- MapDistanceCalculator/MapDistanceCalculator/MainPage.xaml.cs
+++ MapDistanceCalculator/MapDistanceCalculator/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +27,29 @@ namespace MapDistanceCalculator
         public MainPage()
         {
             this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var locator = new Geolocator();
+            locator.DesiredAccuracyInMeters = 50;
+
+            var position = await locator.GetGeopositionAsync();
+            await MyMap.TrySetViewAsync(position.Coordinate.Point, 18D);
+
+           // mySlider.Value = MyMap.ZoomLevel;
+
+        }
+        private void getPositionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Lat1 = MyMap.Center.Position.Latitude;
+            Lon1 = MyMap.Center.Position.Longitude;
+
+        }
+        public static double Lat1;
+        public static double Lon1;
+        public static double Lat2;
+        public static double Lon2;
+        public static char unit;
     }
 }
